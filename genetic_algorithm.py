72aa08038689c5d7004d60269dcd738eda86f2da
stokbronx <s223878@dtu.dk>
2024-09-16 17:16:22 -0300

    working on genetic algorithm

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,7 +1,7 @@
 import random
 from eve_calc import calculate
 class GeneticAlgorithm:
-    def __init__(self, population_size, crossover_rate, mutation_rate, max_generations, selection_method, crossover_method, mutation_method, replacement_strategy):
+    def __init__(self, population_size, crossover_rate, mutation_rate, max_generations, selection_method, crossover_method, mutation_method, replacement_strategy, puntos_a_distribuir, time_limit, personality):
         self.population_size = population_size
         self.crossover_rate = crossover_rate
         self.mutation_rate = mutation_rate
@@ -10,7 +10,12 @@ class GeneticAlgorithm:
         self.selection_method = selection_method
         self.mutation_method = mutation_method
         self.replacement_strategy = replacement_strategy
+        self.puntos_a_distribuir = puntos_a_distribuir
+        self.time_limit = time_limit
+        self.personality = personality
+
         self.population = []
+
 
 
     def initialize_population(self):
@@ -21,14 +26,18 @@ class GeneticAlgorithm:
             self.population.append(individual)
 
     def create_random_individual(self):
+        puntos_a_distribuir = self.puntos_a_distribuir
         # Define a random individual (for now, just random numbers as placeholders)
+        random_numbers = [random.randint(1, 100) for _ in range(5)]
+        total = sum(random_numbers)
+        normalized_numbers = [num / total for num in random_numbers]
         return {
             'height': random.uniform(1.3, 2.0),
-            'strength': random.randint(10, 50),
-            'dexterity': random.randint(10, 50),
-            'intelligence': random.randint(10, 50),
-            'vigor': random.randint(10, 50),
-            'constitution': random.randint(10, 50)
+            'strength': normalized_numbers[0] * puntos_a_distribuir,
+            'dexterity': normalized_numbers[1] * puntos_a_distribuir,
+            'intelligence': normalized_numbers[2] * puntos_a_distribuir,
+            'vigor': normalized_numbers[3] * puntos_a_distribuir,
+            'constitution': normalized_numbers[4] * puntos_a_distribuir
         }
 
     def run(self):
@@ -53,15 +62,16 @@ class GeneticAlgorithm:
 
 
     def evaluate_population(self):
-        # Placeholder: In the future, this will calculate the fitness for each individual
         for individual in self.population:
-            individual['fitness'] = calculate(individual['class'], individual['strength'], individual['dexterity'], individual['intelligence'], individual['vigor'], individual['constitution'], individual['height']   )
+            individual['fitness'] = calculate(individual['personality'], individual['strength'], individual['dexterity'], individual['intelligence'], individual['vigor'], individual['constitution'], individual['height']   )
 
         print("Evaluating population...")
 
     def select_parents(self):
         # Placeholder: This function will select individuals for crossover
         print("Selecting parents...")
+        if self.selection_method == "elite":
+            return self.elite_selection()
         return self.population[:2]  # Just select the first two individuals for now
 
     def create_next_generation(self, parents):
--- main.py
+++ main.py
@@ -12,10 +12,18 @@ def main():
         crossover_rate=config['crossover_rate'],
         mutation_rate=config['mutation_rate'],
         max_generations=config['max_generations'],
-        selection_method=config['selection_method'],
+        selection_method1=config['selection_method1'],
+        selection_method2=config['selection_method2'],
+        selection_ratio=config['selection_ratio'],
         crossover_method=config['crossover_method'],
         mutation_method=config['mutation_method'],
-        replacement_strategy=config['replacement_strategy']
+        replacement_strategy1=config['replacement_strategy1'],
+        replacement_strategy2=config['replacement_strategy2'],
+        replacement_ratio=config['replacement_ratio'],
+        puntos_a_distribuir=config['puntos_a_distribuir'],
+        time_limit=config['time_limit'],
+        personality=config['personality']
+        #The last three to be passed as command line arguments??
     )
 
     # Run the genetic algorithm
